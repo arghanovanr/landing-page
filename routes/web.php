f6443f34fca419a926d8b2b983d1664b607e2869
arghanovanr <arghanovanr@gmail.com>
2023-01-27 07:11:03 +0700

    feat: add navigation bar and heroes section

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/homepage', function () {
     return view('homepage');
 });
+
+
 
 Auth::routes();
 
